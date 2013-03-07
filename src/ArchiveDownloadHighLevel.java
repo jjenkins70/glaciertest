e934569a9741f62601968eeaea121b47f2926a3b
jjenkins70 <jjenkins@jwtechnologies.biz>
2013-03-07 09:59:41 -0500

    updated java files

--- src/ArchiveDownloadHighLevel.java
+++ src/ArchiveDownloadHighLevel.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Scanner;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.PropertiesCredentials;
@@ -7,14 +8,32 @@ import com.amazonaws.services.glacier.AmazonGlacierClient;
 import com.amazonaws.services.glacier.transfer.ArchiveTransferManager;
 
 
+
+
 public class ArchiveDownloadHighLevel {
-    public static String vaultName = "jeremiahtest1";
-    public static String archiveId = "Y577n8dprkXOOCPUI5rLwl9-jmbTHABJaCZJJsgW6Ao8LltmI3H4aPs_Nj-y7G0qfxtlOZ7qWXOU5NMZyhlFNp8HeIJNH51Yz2q_xx2jamB5WeL7Dz4UFhwcMYTruN_Qja9Nd3mtkQ";
-    public static String downloadFilePath  = "/tmp/glacierdownload/";
+	
+
+	
+    //public static String vaultName = "jeremiahtest1";
+    //public static String archiveId = "Y577n8dprkXOOCPUI5rLwl9-jmbTHABJaCZJJsgW6Ao8LltmI3H4aPs_Nj-y7G0qfxtlOZ7qWXOU5NMZyhlFNp8HeIJNH51Yz2q_xx2jamB5WeL7Dz4UFhwcMYTruN_Qja9Nd3mtkQ";
+    //public static String downloadFilePath  = "/tmp/glacierdownload/";
     
     public static AmazonGlacierClient client;
     
     public static void main(String[] args) throws IOException {
+    	
+    	String vault="";
+    	Scanner input=new Scanner(System.in);
+    	System.out.print("Vault Name:>");
+    	vault=input.next();
+    	
+    	String archiveId="";
+    	System.out.print("ArchiveId:>");
+    	archiveId=input.next();
+    	
+    	String downloadFilePath="";
+    	System.out.print("DownloadFilePath:>");
+    	downloadFilePath=input.next();
         
         AWSCredentials credentials = new PropertiesCredentials(
                 ArchiveDownloadHighLevel.class.getResourceAsStream("AwsCredentials.properties"));
@@ -24,7 +43,7 @@ public class ArchiveDownloadHighLevel {
         try {
             ArchiveTransferManager atm = new ArchiveTransferManager(client, credentials);
             
-            atm.download(vaultName, archiveId, new File(downloadFilePath));
+            atm.download(vault, archiveId, new File(downloadFilePath));
             
         } catch (Exception e)
         {
--- src/upload.java
+++ src/upload.java
@@ -8,14 +8,28 @@ import com.amazonaws.services.glacier.AmazonGlacierClient;
 import com.amazonaws.services.glacier.transfer.ArchiveTransferManager;
 import com.amazonaws.services.glacier.transfer.UploadResult;
 
+import java.util.Scanner;
+
 
 public class upload {
-    public static String vaultName = "jeremiahtest1";
-    public static String archiveToUpload = "/home/jjenkins/VirtualBox-4.2-4.2.4_81684_fedora16-1.x86_64.rpm";
+	
+  //  public static String vaultName = "jeremiahtest1";
+	//public static String archiveToUpload = "/home/jjenkins/VirtualBox-4.2-4.2.4_81684_fedora16-1.x86_64.rpm";
     
     public static AmazonGlacierClient client;
     
     public static void main(String[] args) throws IOException {
+    	
+    	//testing
+    	String vault="";
+    	Scanner input=new Scanner(System.in);
+    	System.out.print("Vault Name:>");
+    	vault=input.next();
+    	
+    	String filename="";
+    	System.out.print("File Name:>");
+    	filename=input.next();
+    	
         
         AWSCredentials credentials = new PropertiesCredentials(
                 upload.class.getResourceAsStream("AwsCredentials.properties"));
@@ -25,7 +39,7 @@ public class upload {
         try {
             ArchiveTransferManager atm = new ArchiveTransferManager(client, credentials);
             
-            UploadResult result = atm.upload(vaultName, "my archive " + (new Date()), new File(archiveToUpload));
+            UploadResult result = atm.upload(vault, "my archive " + (new Date()), new File(filename));
             System.out.println("Archive ID: " + result.getArchiveId());
             
         } catch (Exception e)
